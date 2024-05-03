393b2c753baa6981d77fe63d0768a4c62abe2d88
Kamille <kamille.tipan@gmail.com>
2024-05-03 19:58:32 -0400

    Collected 100 messages into an array from Message entities

--- index.js
+++ index.js
@@ -21,19 +21,52 @@ const stringSession = new StringSession("1AQAOMTQ5LjE1NC4xNzUuNTIBu4mtXRPbSY7d2J
   console.log("You should now be connected.");
   console.log(client.session.save()); // Save this string to avoid logging in again
   
-  const result = await client.invoke(
-    new Api.channels.GetMessages({
-      channel: "citycouponmom",
-      id: [399285],
-    })
-  );
-const localState = await client.invoke(
-    new Api.updates.GetState({})
-  );
-  console.log(localState);
-
-// const remoteState = await client.invoke(
-//     new Api.updates.GetState({})
+//   const result = await client.invoke(
+//     new Api.channels.GetMessages({
+//       channel: "citycouponmom",
+//       id: [399285],
+//     })
 //   );
-  console.log(result);
+
+  let offset_id = 0;
+  let limit = 100;
+  let all_messages = [];
+  let total_messages = 0;
+  let total_count_limit = 1;
+
+  while(true){
+    console.log("Current Offset ID is:", offset_id, "; Total Messages:", total_messages);
+    let my_channel = await client.getInputEntity("citycouponmom");
+
+    const history = await client.invoke(
+        new Api.messages.GetHistory({
+        peer: my_channel,
+        offset_id: offset_id,
+        offset_date: null,
+        add_offset: 0,
+        limit: limit,
+        max_id: 0,
+        min_id: 0,
+        hash: 0
+        })
+    );
+
+    if(!history.messages){
+        break;
+    }
+
+    let messages = history.messages;
+
+    for(let i = 0; i < limit; i++){
+        all_messages.push(messages[i].message);
+    }
+    // offset_id = messages[messages.length - 1].id;
+    // total_messages = all_messages.length;
+
+    for(let i = 0; i < all_messages.length; i++){
+        console.log(all_messages[i]);
+    }
+    break;
+}
+
 })();
