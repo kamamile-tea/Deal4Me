1990280a4b9c3cfaeb8491e522267bae1ae7712d
Kamille <kamille.tipan@gmail.com>
2024-05-15 23:39:48 -0400

    Fixed carousel a bit

--- src/app/components/CouponCard.tsx
+++ src/app/components/CouponCard.tsx
@@ -4,13 +4,32 @@ import { CardMedia } from '@mui/material';
 import { CardContent } from '@mui/material';
 import { CardActions } from '@mui/material';
 import { Avatar } from '@mui/material';
+import { Typography } from '@mui/material';
 
-export default function CouponCard({description}:{description:string}) {
+export default function CouponCard(
+    {brandName, expiryDate, description, image}:{brandName:string, expiryDate:string, description:string, image:string}) {
     return (
         <Card sx={{ maxWidth: 345 }}>
             <CardHeader
-                title = {description}
+                avatar={
+                    <Avatar src = "">
+                    R
+                    </Avatar>
+                }
+                title = {brandName}
+                subheader = {"Expires:" + expiryDate}
             />
+            <CardMedia
+                sx={{ height: 100}}
+                component="img"
+                image={image}
+                alt={brandName}
+            />
+            <CardContent>
+                <Typography>
+                    {description}
+                </Typography>
+            </CardContent>
             
 
         </Card>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,30 +5,54 @@ import NavBar from "./components/NavBar";
 import Button from '@mui/material/Button';
 import { useState } from 'react';
 import CouponCard from "./components/CouponCard";
-import CouponCarousel from "./components/CouponCarousel";
 import Carousel from 'react-material-ui-carousel'
+import Grid from '@mui/system/Unstable_Grid';
+import Container from '@mui/material/Container';
 
 export default function Home() {
   const [coupons, setCoupons] = useState([]);
   const [keywordMatches, setKeywordMatches] = useState([]);
+  const [matches, setMatches] = useState([]);
+  let start = 0;
+  let count = 0;
 
   const getCoupons = async () => {
     const res = await fetch('http://localhost:3000/api/couponScraper');
     const response  = await res.json();
     setCoupons(response["filteredCoupons"]);
     setKeywordMatches(response["keywordMatch"]);
-
+    setMatches(response["matches"]);
   }
   return (
       <div>
-         <Button onClick={() => {getCoupons()}}>hello</Button>
-         {keywordMatches.map((keywordMatch) => 
-          <Carousel autoPlay = {false}>
-            {coupons.map((coupon, index) => 
-              <CouponCard key = {index} description={coupon["description"]}/>
-            )}
-          </Carousel>
-         )}
+         <Button onClick={() => {getCoupons()}}>Refresh</Button>
+         
+         {keywordMatches.map((keywordMatch, i) => (
+          <Grid container spacing={2}>
+            <Grid xs={4}>
+              <p>{keywordMatch}</p>
+            </Grid>
+            <Grid xs={8}>
+              <Carousel autoPlay = {false}>
+                {coupons.slice(start, matches[i]+start).map((coupon, index, arr) => (
+                    <CouponCard key = {index}
+                      brandName={coupon["brandName"]}
+                      expiryDate={coupon["expiryDate"]}
+                      description={coupon["description"]}
+                      image={coupon["image"]}
+                    />
+                ))}
+              </Carousel>
+              
+              <p hidden>
+              {start = (matches[i]+start)}
+              </p>
+              
+            </Grid>
+          </Grid>
+          
+         ))}
+         
       </div>
 
   );
